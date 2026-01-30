52f8581f393129b13bf8a3826dc6c6aee59a429d
phs970510-droid <phs970510@gmail.com>
2026-01-30 13:29:34 +0900

    2

--- Assets/WorkSpace/PHS/Scripts/EffectManager.cs
+++ Assets/WorkSpace/PHS/Scripts/EffectManager.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class EffectManager : MonoBehaviour
 {
-    public static EffectManager Instance { get; private set; }
-
-    private void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this; DontDestroyOnLoad(gameObject);
-        }
-        else
-            Destroy(gameObject);
-    }
+    
 }
