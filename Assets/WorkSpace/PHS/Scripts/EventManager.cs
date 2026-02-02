75e26bf0efcfbb435b810b2b3296044bb25489a2
phs970510-droid <phs970510@gmail.com>
2026-02-02 10:57:01 +0900

    eventmanager

--- Assets/WorkSpace/PHS/Scripts/EventManager.cs
+++ Assets/WorkSpace/PHS/Scripts/EventManager.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class EventManager : MonoBehaviour
 {
     public static EventManager Instance { get; private set; }
+
+    private Dictionary<string, UnityAction> eventDict;
 
     private void Awake()
     {
@@ -14,5 +17,40 @@ public class EventManager : MonoBehaviour
         }
         else
             Destroy(gameObject);
+    }
+
+    public void StartList(string eventName, UnityAction list)
+    {
+        UnityAction thisEvent;
+        if (eventDict.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent += list;
+            eventDict[eventName] = thisEvent;
+        }
+        else
+        {
+            thisEvent += list;
+            eventDict.Add(eventName, thisEvent);
+        }
+    }
+
+    public void StopList(string eventName, UnityAction list)
+    {
+        if(Instance==null)return;
+        UnityAction thisEvent;
+        if (eventDict.TryGetValue(eventName, out thisEvent)) 
+        { 
+            thisEvent -= list;
+            eventDict[eventName] = thisEvent;
+        }
+    }
+
+    public void TriggerEvent(string eventName)
+    {
+        UnityAction thisEvent;
+        if (eventDict.TryGetValue(eventName, out thisEvent))
+        { 
+            thisEvent.Invoke();
+        }
     }
 }
