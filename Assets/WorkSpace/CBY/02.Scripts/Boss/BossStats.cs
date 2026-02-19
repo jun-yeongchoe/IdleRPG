a0b30c08a2202e66e5cba00f14019004e6923081
BigMan <cju0253@gmail.com>
2026-02-19 19:55:18 +0900

    수정 3

--- Assets/WorkSpace/CBY/02.Scripts/Boss/BossStats.cs
+++ Assets/WorkSpace/CBY/02.Scripts/Boss/BossStats.cs
@@ -2,41 +2,35 @@ using UnityEngine;
 
 public class BossStats : MonoBehaviour
 {
-    public BossStatSO statData;
+    [Header("Base Stats")]
+    public float baseHp = 300f;
+    public float hpPerStage = 50f;
 
-    [Header("Runtime Stats")]
-    public float maxHp;
-    public float hp;
-    public float attack;
-    public float moveSpeed;
-    public float attackRange;
-    public float attackCooldown;
+    public float baseAttack = 20f;
+    public float attackPerStage = 5f;
+
+    public float moveSpeed = 2f;
+    public float attackRange = 2f;
+    public float attackCooldown = 2f;
+
+    [HideInInspector] public float maxHp;
+    [HideInInspector] public float currentHp;
+    [HideInInspector] public float attackDamage;
 
     public void InitByStage()
     {
-        int stage = DataManager.Instance.currentStageNum;
+        int stage = 0;
 
-        maxHp = statData.baseHp + stage * statData.hpPerStage;
-        attack = statData.baseAttack + stage * statData.attackPerStage;
-        moveSpeed = statData.baseMoveSpeed;
+        if (DataManager.Instance != null)
+            stage = DataManager.Instance.currentStageNum;
 
-        attackRange = statData.attackRange;
-        attackCooldown = statData.attackCooldown;
-
-        hp = maxHp;
+        maxHp = baseHp + stage * hpPerStage;
+        attackDamage = baseAttack + stage * attackPerStage;
+        currentHp = maxHp;
     }
 
-    //보스가 데미지를 입는 함수
     public void TakeDamage(float damage)
     {
-        hp -= damage;
-        if (hp <= 0)
-            Die();
-    }
-
-    void Die()
-    {
-        Debug.Log("Boss Defeated!");
-        gameObject.SetActive(false);
+        currentHp -= damage;
     }
 }
