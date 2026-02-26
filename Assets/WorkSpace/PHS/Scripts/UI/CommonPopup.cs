a8b7252dede67e1052d0c61ae65504068cfc60bb
phs970510-droid <phs970510@gmail.com>
2026-02-26 17:38:32 +0900

    Update CommonPopup.cs

--- Assets/WorkSpace/PHS/Scripts/UI/CommonPopup.cs
+++ Assets/WorkSpace/PHS/Scripts/UI/CommonPopup.cs
@@ -21,18 +21,18 @@ public class CommonPopup : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject); // 씬 넘어가도 살아있게
+            DontDestroyOnLoad(gameObject); //씬 넘어가도 살아있게
         }
         else Destroy(gameObject);
 
-        // 버튼 리스너
+        //버튼 리스너
         confirmButton.onClick.AddListener(OnConfirm);
         if (cancelButton != null) cancelButton.onClick.AddListener(OnCancel);
 
-        contentObj.SetActive(false); // 시작할 땐 꺼둠
+        contentObj.SetActive(false); //시작할 땐 꺼둠
     }
 
-    // 1. 확인 버튼만 있는 알림창 (예: 돈 부족함!)
+    //확인 버튼만 있는 알림창 (예: 돈 부족함!)
     public void ShowAlert(string title, string body, string btnText = "확인", Action onConfirm=null)
     {
         titleText.text = title;
@@ -45,22 +45,22 @@ public class CommonPopup : MonoBehaviour
         contentObj.SetActive(true);
     }
 
-    // 2. 예/아니오 선택창 (예: 구매하시겠습니까?)
+    //예/아니오 선택창
     public void ShowConfirm(string title, string body, Action onConfirm, string btnText = "네")
     {
         titleText.text = title;
         bodyText.text = body;
         confirmText.text = btnText;
 
-        onConfirmCallback = onConfirm; // 실행할 함수 저장
+        onConfirmCallback = onConfirm; //실행할 함수 저장
 
-        if (cancelButton != null) cancelButton.gameObject.SetActive(true); // 취소 버튼 보임
+        if (cancelButton != null) cancelButton.gameObject.SetActive(true); //취소 버튼 보임
         contentObj.SetActive(true);
     }
 
     private void OnConfirm()
     {
-        onConfirmCallback?.Invoke(); // 저장된 함수 실행
+        onConfirmCallback?.Invoke(); //저장된 함수 실행
         Close();
     }
 
