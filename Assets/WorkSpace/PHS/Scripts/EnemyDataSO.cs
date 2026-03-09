bc1bedac4e3a54b90a40ebc12d2932553e71cbd3
phs970510-droid <phs970510@gmail.com>
2026-03-09 12:00:56 +0900

    enemy

---
+++ Assets/WorkSpace/PHS/Scripts/EnemyDataSO.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="EnemyData", menuName ="Enemy Data")]
+public class EnemyDataSO : ScriptableObject
+{
+    [Header("Basic")]
+    public string enemyName = "Default";
+
+    [Header("Stats")]
+    public int maxHp=100;
+    public float attackPower=2f;
+    public float moveSpeed=1.5f;
+    public float attackRange=1.5f;
+    public float attackCooldown = 1.5f;
+}
